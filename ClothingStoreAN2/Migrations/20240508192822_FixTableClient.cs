c49bbc8443820e932789aa1da5d7d5253661d2d1
Anastasia Krasnova <krasnovaanastasia133@gmail.com>
2024-05-08 21:30:06 +0200

    AddMigrations + fix Table Client

---
+++ ClothingStoreAN2/Migrations/20240508192822_FixTableClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClothingStoreAN2.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTableClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientPhotoId",
+                table: "Client");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientPhotoId",
+                table: "Client",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
